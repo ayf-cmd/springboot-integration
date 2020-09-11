e3bf3d5958b0c04bea74d1fe6be5545ac414db17
anyunfeng <anyunfeng@fintell.com.cn>
2020-09-11 13:29:25 +0800

    优化mybatis、mq增加查询堆积的方法

--- rocketmq-common/src/main/java/com/rocketmq/mq/producer/RocketMqProducer.java
+++ rocketmq-common/src/main/java/com/rocketmq/mq/producer/RocketMqProducer.java
@@ -6,11 +6,18 @@ import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendCallback;
 import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.admin.ConsumeStats;
+import org.apache.rocketmq.common.admin.OffsetWrapper;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
 import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.apache.rocketmq.tools.admin.DefaultMQAdminExt;
 
 import javax.annotation.PostConstruct;
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -19,6 +26,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Slf4j
 public class RocketMqProducer implements Serializable{
     private volatile DefaultMQProducer producer;
+    private DefaultMQAdminExt defaultMQAdminExt;
     private RocketMqProperties rocketMqProperties;
     private static AtomicInteger messageCount = new AtomicInteger(0);
 
@@ -37,7 +45,55 @@ public class RocketMqProducer implements Serializable{
         }
         return this.producer;
     }
-    
+
+    @PostConstruct
+    private DefaultMQAdminExt getInstanceAdmin() {
+        if (this.defaultMQAdminExt == null) {
+            synchronized(this) {
+                if (this.defaultMQAdminExt == null) {
+                    initializeAdminExt();
+                }
+            }
+        }
+        return this.defaultMQAdminExt;
+    }
+
+    /**
+     * 获取topic的消息堆积量
+     */
+    public synchronized long getMsgPileNum(){
+        long diffTotal = 0L;
+        try{
+            // 当消费端未消费时，此方法会报错
+            ConsumeStats consumeStats = this.getInstanceAdmin().examineConsumeStats(rocketMqProperties.getProducer().getGroupName());
+            List<MessageQueue> mqList = new LinkedList();
+            mqList.addAll(consumeStats.getOffsetTable().keySet());
+            Collections.sort(mqList);
+            // 遍历所有的队列，计算堆积量
+            for (MessageQueue mq : mqList) {
+                if(rocketMqProperties.getProducer().getTopic().equals(mq.getTopic())){
+                    OffsetWrapper offsetWrapper = (OffsetWrapper)consumeStats.getOffsetTable().get(mq);
+                    diffTotal += (offsetWrapper.getBrokerOffset() - offsetWrapper.getConsumerOffset());
+                }
+            }
+            return diffTotal;
+        }catch(Throwable e){
+            return -1L;
+        }
+    }
+
+    // 初始化监控客户端
+    private void initializeAdminExt() {
+        try {
+            defaultMQAdminExt = new DefaultMQAdminExt();
+            defaultMQAdminExt.setInstanceName(Long.toString(System.currentTimeMillis()));
+            defaultMQAdminExt.setNamesrvAddr(rocketMqProperties.getNamesrvAddr());
+            defaultMQAdminExt.start();
+        }catch (Exception e){
+            log.error("监控客户端启动失败...", e);
+        }
+    }
+
     private void initialize() {
         producer = new DefaultMQProducer(rocketMqProperties.getProducer().getGroupName());
         producer.setNamesrvAddr(rocketMqProperties.getNamesrvAddr());
@@ -90,7 +146,13 @@ public class RocketMqProducer implements Serializable{
             } catch (Exception e) {
                 log.error(e.getMessage(), e);
             }
+            try {
+                defaultMQAdminExt.shutdown();
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
             this.producer = null;
+            this.defaultMQAdminExt = null;
         }
     }
     
