dd25166a0789315774531d3363cbb21b6796f04d
anyunfeng <anyunfeng@fintell.com.cn>
2020-09-16 16:46:02 +0800

    增加kafka

---
+++ kafka-test/src/main/java/com/kafka/KafkaApplication.java
@@ -0,0 +1,17 @@
+package com.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@ServletComponentScan
+@EnableAsync
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+}
